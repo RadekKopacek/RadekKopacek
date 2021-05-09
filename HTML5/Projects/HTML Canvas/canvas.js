086f908ccde77c09c6f8416328f0336f48a537a5
threeDolphs <radek.kopacek@gmail.com>
2021-05-09 22:23:01 +0100

    more canvas

--- HTML5/Projects/HTML Canvas/canvas.js
+++ HTML5/Projects/HTML Canvas/canvas.js
@@ -2,7 +2,9 @@
 // 2 - set properties/parameters
 // 3 - actions (make shape or line)
 function makeCanvas(){
-    // Canvas 1  - TEXT
+    var PI_TWO = Math.PI * 2;
+
+    ///// Canvas 1  - TEXT  /////
     // 1 - get object
     var canvas1 = document.getElementById('canvas1');
     var ctx1 = canvas1.getContext('2d');
@@ -16,7 +18,7 @@ function makeCanvas(){
     ctx1.fillText('I Love HTML',80,150);
     ctx1.strokeText('I Love HTML',80,150);
 
-    // Canvas 2  -  Squares
+    ////// Canvas 2  -  Squares //////
     // 1 - get object
     var canvas2 = document.getElementById('canvas2');
     var ctx2 = canvas2.getContext('2d');
@@ -39,4 +41,85 @@ function makeCanvas(){
     ctx2.fillStyle = "#ff0000";
     ctx2.fillRect(200,150,135,135);
     ctx2.strokeRect(200,150,135,135);
+
+    ///// Canvas 3 - Lines////
+    // 1 - get object
+    var canvas3 = document.getElementById('canvas3');
+    var ctx3 = canvas3.getContext('2d');
+
+    ctx3.strokeStyle = "#666666";
+    ctx3.fillStyle = "#ff0000";
+    ctx3.lineWidth = 5;
+
+    ctx3.beginPath();
+    ctx3.moveTo(100,100);
+    ctx3.lineTo(150,200);
+    ctx3.lineTo(200,200);
+    ctx3.lineTo(200,290);
+    ctx3.lineTo(290,290);
+    ctx3.lineTo(290,100);
+    ctx3.lineTo(98,100);
+    ctx3.stroke();
+    ctx3.fill();
+    ctx3.closePath();
+
+    ///// Canvas 4 - Circles////
+    // 1 - get object
+    var canvas4 = document.getElementById('canvas4');
+    var ctx4 = canvas4.getContext('2d');
+
+    ctx4.fillStyle = "blue";
+
+    ctx4.beginPath();
+    ctx4.arc(200,30,25,0,PI_TWO);
+    ctx4.fill();
+    ctx4.closePath();
+
+    ctx4.fillStyle = "red";
+
+    ctx4.beginPath();
+    ctx4.arc(200,100,45,0,PI_TWO);
+    ctx4.fill();
+    ctx4.closePath();
+
+    ctx4.fillStyle = "black";
+
+    ctx4.beginPath();
+    ctx4.arc(200,220,75,0,PI_TWO);
+    ctx4.fill();
+    ctx4.closePath();
+
+    ///// Canvas 5 - Animation////
+    // 1 - get object
+    var canvas5 = document.getElementById('canvas5');
+    var ctx5 = canvas5.getContext('2d');
+
+    var posX = 0;
+    var posY = 0;
+    setInterval(function(){
+        posX += 1;
+        posY += 1;
+        ctx5.fillStyle = "black";
+        ctx5.fillRect(0,0,canvas5.width,canvas5.height);
+
+        ctx5.fillStyle = "white";
+        ctx5.beginPath();
+        ctx5.arc(posX,120,55,0,PI_TWO);
+        ctx5.fill();
+        ctx5.closePath();
+
+        ctx5.fillStyle = "red";
+        ctx5.beginPath();
+        ctx5.arc(150,posY,55,0,PI_TWO);
+        ctx5.fill();
+        ctx5.closePath();
+
+        ctx5.fillStyle = "blue";
+        ctx5.beginPath();
+        ctx5.arc(350,posY,55,0,PI_TWO);
+        ctx5.fill();
+        ctx5.closePath();
+
+    },30);
+
 }
