6469554bb01889ad9935cf6df54855186a76f7e6
threeDolphs <radek.kopacek@gmail.com>
2021-09-24 22:35:51 +0100

    js conditional

---
+++ Bootcamp 2021/Projects/C-20 First HTML Page/javascript/Conditionals/app.js
@@ -0,0 +1,37 @@
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
+// } else {
+//     console.log("YOUR NUMBER IS GREATER (OR EQUAL) THEN 0.5!")
+// }
+// console.log(random);
+
+
+// const dayOfWeek = prompt('ENTER A DAY').toLowerCase();
+
+// if (dayOfWeek === 'monday') {
+//     console.log("UGHhHH I HATE MONDAYS!")
+// } else if (dayOfWeek === 'saturday') {
+//     console.log("YAY I LOVE SATURDAYS!")
+// } else if (dayOfWeek === 'friday') {
+//     console.log("patek blazni maji svatek")
+// } else {
+//     console.log("MEH")
+// }
+
+// 0 - 5 - FREE
+// 5 - 10 - CHILD $10
+// 10 - 65 - ADULT $20
+// 65+ - SENIOR $10
+
+const age = 48;
+
+if (age < 5) {
+    console.log("You are a baby. You get in for free!")
+} else if (age < 10) {
+    console.log("You are a child. You pay $10!")
+} else if (age < 65) {
+    console.log("You are an adult. You pay $20!")
+} else {
+    console.log("You are a senior. You pay $10!")
+}
