6c33ccb3ac417dd3cf2d48b8da788ba8e12a5ffc
threeDolphs <radek.kopacek@gmail.com>
2021-09-25 22:04:52 +0100

    more conditionals

--- Bootcamp 2021/Projects/C-20 First HTML Page/javascript/Conditionals/app.js
+++ Bootcamp 2021/Projects/C-20 First HTML Page/javascript/Conditionals/app.js
@@ -24,14 +24,30 @@
 // 10 - 65 - ADULT $20
 // 65+ - SENIOR $10
 
-const age = 48;
+// const age = 48;
 
-if (age < 5) {
-    console.log("You are a baby. You get in for free!")
-} else if (age < 10) {
-    console.log("You are a child. You pay $10!")
-} else if (age < 65) {
-    console.log("You are an adult. You pay $20!")
+// if (age < 5) {
+//     console.log("You are a baby. You get in for free!")
+// } else if (age < 10) {
+//     console.log("You are a child. You pay $10!")
+// } else if (age < 65) {
+//     console.log("You are an adult. You pay $20!")
+// } else {
+//     console.log("You are a senior. You pay $10!")
+// }
+
+const password = prompt("please enter a new password");
+
+// Password must be 6+ characters
+if (password.length >= 6) {
+    // Password cannot include space
+    if (password.indexOf(' ') === -1) {
+        console.log("Valid Password!")
+    } else {
+        console.log("Password cannot contain spaces!")
+    }
 } else {
-    console.log("You are a senior. You pay $10!")
+    console.log("PASSWORD TOO SHORT! MUST BE 6+ CHARACTERS")
 }
+
+
