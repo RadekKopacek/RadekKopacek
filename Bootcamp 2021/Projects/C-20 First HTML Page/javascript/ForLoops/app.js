fb7d7d4e77c4d759820b926b5aa769ae53763f78
threeDolphs <radek.kopacek@gmail.com>
2021-09-29 22:36:29 +0100

    forLoops

---
+++ Bootcamp 2021/Projects/C-20 First HTML Page/javascript/ForLoops/app.js
@@ -0,0 +1,20 @@
+// console.log('Hi');
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 0; i <= 20; i += 2) {
+//     console.log(i);
+// }
+
+// for (let i = 100; i >= 0; i -= 10) {
+//     console.log(i);
+// }
+
+const animals = ['rabbit', 'cat', 'dog', 'horse', 'cow', 'sheep'];
+for (let i = 0; i < animals.length; i++) {
+    console.log(i, animals[i]);
+}
+for (let i = animals.length - 1; i >= 0; i--) {
+    console.log(i, animals[i]);
+}
