7cb24f8708087d677c05549f9938fff06c2e1d2b
threeDolphs <radek.kopacek@gmail.com>
2021-10-04 21:32:06 +0100

    basic functions

---
+++ Bootcamp 2021/Projects/C-20 First HTML Page/javascript/Functions/Basics/app.js
@@ -0,0 +1,16 @@
+// function singSong() {
+//     console.log('DO');
+//     console.log('RE');
+//     console.log('MI');
+// }
+
+// singSong();
+
+function rant(message) {
+    for (let i = 1; i <= 3; i++) {
+        console.log(message.toUpperCase());
+    }
+}
+
+rant('bla bla bal');
+
