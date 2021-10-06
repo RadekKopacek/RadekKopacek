66357108ec72779d453aabc91079135f9faa4a6f
threeDolphs <radek.kopacek@gmail.com>
2021-10-06 21:44:57 +0100

    more function learning

--- Bootcamp 2021/Projects/C-20 First HTML Page/javascript/Functions/Basics/app.js
+++ Bootcamp 2021/Projects/C-20 First HTML Page/javascript/Functions/Basics/app.js
@@ -1,8 +1,8 @@
-// function singSong() {
-//     console.log('DO');
-//     console.log('RE');
-//     console.log('MI');
-// }
+function singSong() {
+    console.log('DO');
+    console.log('RE');
+    console.log('MI');
+}
 
 // singSong();
 
@@ -12,5 +12,27 @@ function rant(message) {
     }
 }
 
-rant('bla bla bal');
+// rant('bla bla bal');
 
+function greet(firstName, lastName) {
+    console.log(`Hello ${firstName} ${lastName[0]}.`)
+}
+
+// greet('Sam', 'Hawkins');
+
+function repeat(str, numTimes) {
+    let result = '';
+    for (let i = 0; i < numTimes; i++) {
+        result += str;
+    }
+    console.log(result);
+}
+
+// repeat('cats ', 20);
+
+function add(x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        return false;
+    }
+    return x + y;
+}
