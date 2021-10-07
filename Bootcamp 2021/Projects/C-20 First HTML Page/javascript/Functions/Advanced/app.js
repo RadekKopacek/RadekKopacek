61514fbefb505d11cd479411031df8823be6c3c8
threeDolphs <radek.kopacek@gmail.com>
2021-10-07 22:05:16 +0100

    functions scope

---
+++ Bootcamp 2021/Projects/C-20 First HTML Page/javascript/Functions/Advanced/app.js
@@ -0,0 +1,40 @@
+// let totalEggs = 0;
+// function collectEggs() {
+//     totalEggs = 6;
+// }
+// console.log(totalEggs);
+// collectEggs();
+// console.log(totalEggs);
+
+// let bird = 'Scarlet Macaw';
+// function birdWatch() {
+//     let bird = 'Great Blue Heron';
+//     console.log(bird);
+// }
+// birdWatch();
+
+// let radius = 8;
+// if (radius > 0) {
+//     const PI = 3.14159;
+//     let msg = 'HIIIII';
+// }
+
+// console.log(radius);
+// console.log(msg);
+
+// function bankRobbery() {
+//     const heroes = ['Spiderman', 'Wolverine', 'Black Panther', 'Batwoman'];
+//     function cryForHelp() {
+//         function inner() {
+//             for (let hero of heroes) {
+//                 console.log(`PLEASE HELP US, ${hero.toUpperCase()}`)
+//             }
+//         }
+//         inner();
+//     }
+//     cryForHelp();
+// }
+
+const add = function (x, y) {
+    return x + y;
+}
