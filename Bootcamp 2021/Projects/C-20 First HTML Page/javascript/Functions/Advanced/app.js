fc52a29785872c4441463619d56729dc5b20ef0f
threeDolphs <radek.kopacek@gmail.com>
2021-10-09 22:09:37 +0100

    this word

--- Bootcamp 2021/Projects/C-20 First HTML Page/javascript/Functions/Advanced/app.js
+++ Bootcamp 2021/Projects/C-20 First HTML Page/javascript/Functions/Advanced/app.js
@@ -85,12 +85,21 @@
 //     return num >= 1 && num <= 10;
 // }
 
-const myMath = {
-    PI: 3.14159,
-    square(num) {
-        return num * num;
-    },
-    cube(num) {
-        return num ** 3;
+// const myMath = {
+//     PI: 3.14159,
+//     square(num) {
+//         return num * num;
+//     },
+//     cube(num) {
+//         return num ** 3;
+//     }
+// }
+
+const cat = {
+    name: 'Blue Steele',
+    color: 'grey',
+    breed: 'scottish fold',
+    meow() {
+        console.log(`${this.name} says MEOW MEOW MEOW`)
     }
 }
