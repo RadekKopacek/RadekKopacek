e636822d1d453933c6bf4c914470bc540b5cbb50
threeDolphs <radek.kopacek@gmail.com>
2021-10-15 21:54:37 +0100

    Default params and spread

---
+++ Bootcamp 2021/Projects/C-20 First HTML Page/javascript/New Features/Spread/app.js
@@ -0,0 +1,12 @@
+// let nums = [13, 5, 6, 9, 1, 3, 23, 34, 567, 9];
+
+// console.log(Math.max(...nums));
+// console.log(Math.min(...nums));
+// console.log(...nums);
+// console.log(nums);
+// console.log(...'hello');
+
+const cats = ['Blue', 'Scout', 'Rocket'];
+const dogs = ['Rusty', 'Wyatt'];
+
+const allPets = [...cats, ...dogs];
