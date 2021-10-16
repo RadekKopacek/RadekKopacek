c99afb540a5ca6a05caf7fd76aeb1877849cdb46
threeDolphs <radek.kopacek@gmail.com>
2021-10-16 20:27:36 +0100

    spread object

--- Bootcamp 2021/Projects/C-20 First HTML Page/javascript/New Features/Spread/app.js
+++ Bootcamp 2021/Projects/C-20 First HTML Page/javascript/New Features/Spread/app.js
@@ -10,3 +10,14 @@ const cats = ['Blue', 'Scout', 'Rocket'];
 const dogs = ['Rusty', 'Wyatt'];
 
 const allPets = [...cats, ...dogs];
+
+const feline = { legs: 4, family: 'Felidae' };
+const canine = { isFurry: true, family: 'Caninae' };
+
+
+const dataFromForm = {
+    email: 'blueman@gmail.com',
+    password: 'tobias123!',
+    username: 'tfunke',
+}
+const newUser = { ...dataFromForm, id: 2345, isAdmin: false }
