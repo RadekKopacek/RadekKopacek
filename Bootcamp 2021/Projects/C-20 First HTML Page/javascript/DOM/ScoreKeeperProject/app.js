59715cd59bc8591d3d667920ead90bbcdcffc0fd
threeDolphs <radek.kopacek@gmail.com>
2021-11-05 21:36:39 +0000

    EventDelegating + Score Keeper project

---
+++ Bootcamp 2021/Projects/C-20 First HTML Page/javascript/DOM/ScoreKeeperProject/app.js
@@ -0,0 +1,5 @@
+const player1 = document.querySelector('#button1');
+const score1 = document.querySelector('#score1');
+player1.addEventListener('click', function () {
+    alert('button1 clicked');
+})
