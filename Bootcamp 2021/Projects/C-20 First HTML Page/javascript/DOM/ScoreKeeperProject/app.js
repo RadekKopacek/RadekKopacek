c8d3c31030e29b0e9e42e0a87fd5d92d68d375a5
threeDolphs <radek.kopacek@gmail.com>
2021-11-09 22:44:53 +0000

    Score Keeper Project

--- Bootcamp 2021/Projects/C-20 First HTML Page/javascript/DOM/ScoreKeeperProject/app.js
+++ Bootcamp 2021/Projects/C-20 First HTML Page/javascript/DOM/ScoreKeeperProject/app.js
@@ -1,5 +1,42 @@
-const player1 = document.querySelector('#button1');
-const score1 = document.querySelector('#score1');
-player1.addEventListener('click', function () {
-    alert('button1 clicked');
+const reset = document.querySelector('#reset');
+reset.addEventListener('click', function () {
+    score1.innerText = 0;
+    score2.innerText = 0;
+    score1.style.color = 'black';
+    score2.style.color = 'black';
+    player1.disabled = false;
+    player2.disabled = false;
 })
+
+const maxScore = document.querySelector('select');
+maxScore.addEventListener('input', function () {
+    console.log(`maxScore = ${maxScore.value}`);
+})
+
+
+const player1 = document.querySelector('#button1');
+let score1 = document.querySelector('#score1');
+player1.addEventListener('click', function () {
+    score1.innerText++;
+    if (score1.innerText === maxScore.value) {
+        score1.style.color = '#4ceb13';
+        score2.style.color = 'red';
+        player1.disabled = true;
+        player2.disabled = true;
+    }
+})
+
+const player2 = document.querySelector('#button2');
+let score2 = document.querySelector('#score2');
+player2.addEventListener('click', function () {
+    score2.innerText++;
+    if (score2.innerText === maxScore.value) {
+        score2.style.color = '#4ceb13';
+        score1.style.color = 'red';
+        player1.disabled = true;
+        player2.disabled = true;
+    }
+})
+
+
+
