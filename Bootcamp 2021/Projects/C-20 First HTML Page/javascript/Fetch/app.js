a5c0f0707e6ab7e25e37c66ad0d4f30f89363ab5
threeDolphs <radek.kopacek@gmail.com>
2021-11-18 21:23:13 +0000

    axios

--- Bootcamp 2021/Projects/C-20 First HTML Page/javascript/Fetch/app.js
+++ Bootcamp 2021/Projects/C-20 First HTML Page/javascript/Fetch/app.js
@@ -12,12 +12,29 @@
 //         console.log("Error!!!", e)
 //     })
 
+// const fetchBitcoinPrice = async () => {
+//     try {
+//         const res = await fetch('https://api.cryptonator.com/api/ticker/btc-usd')
+//         const data = await res.json();
+//         console.log(data.ticker.price)
+//     } catch (e) {
+//         console.log("SOMETHING WENT WRONG!!!", e)
+//     }
+// }
+
+axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+    .then(res => {
+        console.log(res.data.ticker.price)
+    })
+    .catch(err => {
+        console.log("ERROR!!!", err)
+    })
+
 const fetchBitcoinPrice = async () => {
     try {
-        const res = await fetch('https://api.cryptonator.com/api/ticker/btc-usd')
-        const data = await res.json();
-        console.log(data.ticker.price)
+        const res = await axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+        console.log(res.data.ticker.price)
     } catch (e) {
-        console.log("SOMETHING WENT WRONG!!!", e)
+        console.log("ERROR!!!", e)
     }
 }
