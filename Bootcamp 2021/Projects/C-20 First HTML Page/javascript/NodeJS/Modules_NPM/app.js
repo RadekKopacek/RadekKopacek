e46790699f5880cfc020ee9ae28c470119b7f7c8
threeDolphs <radek.kopacek@gmail.com>
2021-11-29 21:38:00 +0000

    more nodeJS

---
+++ Bootcamp 2021/Projects/C-20 First HTML Page/javascript/NodeJS/Modules_NPM/app.js
@@ -0,0 +1,7 @@
+const { PI, square, add } = require('./math');
+// console.log(PI);
+// console.log(square(9));
+// console.log(add(123, 432));
+
+const cats = require('./shelter');
+console.log("REQUIRED AN ENTIRE DIRECTORY!", cats);
---
+++ Bootcamp 2021/Projects/C-20 First HTML Page/javascript/NodeJS/Modules_NPM/math.js
@@ -0,0 +1,15 @@
+exports.add = (x, y) => x + y;
+
+exports.PI = 3.14159;
+
+exports.square = x => x * x;
+
+// const math = {
+//     add: add,
+//     PI: PI,
+// }
+// module.exports = math;
+
+// module.exports.add = add;
+// module.exports.PI = PI;
+// module.exports.square = square;
