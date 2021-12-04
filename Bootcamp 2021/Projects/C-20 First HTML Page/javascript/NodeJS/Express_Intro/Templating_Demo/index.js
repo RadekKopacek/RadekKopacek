7f6145070085eef3b8ec5b447037371fec062478
threeDolphs <radek.kopacek@gmail.com>
2021-12-04 21:00:19 +0000

    more templating

--- Bootcamp 2021/Projects/C-20 First HTML Page/javascript/NodeJS/Express_Intro/Templating_Demo/index.js
+++ Bootcamp 2021/Projects/C-20 First HTML Page/javascript/NodeJS/Express_Intro/Templating_Demo/index.js
@@ -1,1 +1,12 @@
-const
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send("HI");
+});
+
+app.listen(3000, () => {
+    console.log("LISTENING IN PORT 3000....")
+});
